29bd139987b70f863ca45869471e9639ed4df4e7
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2026-06-03 10:37:30 +0800

    feat: added scripts to install the required domains

---
+++ scripts/install-domains.ps1
@@ -0,0 +1,20 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Running as administrator..." -ForegroundColor Yellow
+    # Relaunch the script with elevated privileges
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+
+# 2. Define the hosts file path and the entries to add
+$hostsPath = "$env:windir\System32\drivers\etc\hosts"
+$hostsEntries = @"
+127.0.0.1 storage.unify.com
+127.0.0.1 s3-console.unify.com
+127.0.0.1 unify.com
+"@
+
+# 3. Append the entries to the hosts file
+Add-Content -Path $hostsPath -Value $hostsEntries
+
+Write-Host "Installed domains successfully" -ForegroundColor Green
---
+++ scripts/install-domains.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ "$EUID" -ne 0 ]]; then
+  echo Running as root
+  exec sudo $0
+fi
+
+tee -a /etc/hosts > /dev/null << EOF 
+127.0.0.1 storage.unify.com
+127.0.0.1 s3-console.unify.com
+127.0.0.1 unify.com
+EOF
+
+echo Installed domains successfully
