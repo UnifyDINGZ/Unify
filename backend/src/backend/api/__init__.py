b64c7701dd1030846f629034adc7eb18286ba8cc
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2026-06-10 10:41:20 +0800

    feat: added healthchecks to backend services

--- backend/src/backend/api/__init__.py
+++ backend/src/backend/api/__init__.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 router = FastAPI()
 
 
+@router.get("/ping", tags=["Misc"])
+async def ping() -> str:
+    """
+    Pings the backend. Internally used
+    as a healthcheck.
+    """
+    return f"Pong! Head to the docs at {router.docs_url or "<<Unknown>>"} to see the backend's functionality"
+
+
 @router.get("/greet/{name}")
 async def greet(name: str) -> str:
     """
