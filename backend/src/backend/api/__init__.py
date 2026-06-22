5eeac2e40e082767d3c3b97e7633ea18251f4a84
Kiwifuit <75169058+Kiwifuit@users.noreply.github.com>
2026-06-22 18:12:38 +0800

    chore: ping!

--- backend/src/backend/api/__init__.py
+++ backend/src/backend/api/__init__.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
+from os import environ
 
 router = FastAPI()
 
 
 @router.get("/ping", tags=["Misc"])
-async def ping() -> str:
+async def ping() -> dict:
     """
     Pings the backend. Internally used
     as a healthcheck.
     """
-    return f"Pong! Head to the docs at {router.docs_url or "<<Unknown>>"} to see the backend's functionality"
-
-
-@router.get("/greet/{name}")
-async def greet(name: str) -> str:
-    """
-    Returns a string greeting `user`
-    """
-    return f"Hello, {name}!!"
+    return {
+        "response": f"Pong! Head to the docs at {router.docs_url or "<<Unknown>>"} to see the backend's functionality",
+        "environment": dict(environ),
+    }
